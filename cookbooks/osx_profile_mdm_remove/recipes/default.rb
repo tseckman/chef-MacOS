653be7bb1b65fe206c9b49ed837ece1584f74c3f
Seckman, Thomas (US) <tseckman@cyberpointllc.com>
2016-11-30 14:56:58 -0500

    made change to mdm remove recipe

--- cookbooks/osx_profile_mdm_remove/recipes/default.rb
+++ cookbooks/osx_profile_mdm_remove/recipes/default.rb
@@ -6,7 +6,7 @@
 
 # Remove any MDM profiles from Profile Manager
 
-execute "Remove ALL Profiles" do
+execute "Remove ALL Apple MDM Profiles" do
 	command '/bin/echo y|/usr/bin/profiles -D'
 	only_if '/usr/bin/profiles -P | /usr/bin/grep com.apple.mdm'
 end
